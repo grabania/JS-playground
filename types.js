fc3368265e31baee966c27c86dd6e3c5901ae290
grabania <grabania.work@gmail.com>
2020-07-13 23:45:51 +0100

    Add strings to types

--- types.js
+++ types.js
@@ -1,1 +1,16 @@
-console.log('it works');
+const name = 'Bob';
+const middle = 'the';
+const last = `Builder`;
+
+const sentence = 'she\'s so "cool"';
+const sentence2 = `she's so "cool"`;
+
+const song = `Ohhh
+
+ya
+
+I like 
+
+pizza`;
+
+const hello = `hello my name is ${name}. Nice to meet you`;
