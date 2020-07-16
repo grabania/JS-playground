79137260e11a7ac4edca9854230e17752c4f7486
grabania <grabania.work@gmail.com>
2020-07-17 00:06:57 +0100

    Add function definition

---
+++ custom-functions/cf.js
@@ -0,0 +1,8 @@
+//Function Definition
+function calculateBill() {
+    //this is the function body(or block)
+    console.log('Running Calculate Bill!!');
+}
+
+//Function Call or Function Run
+calculateBill();
