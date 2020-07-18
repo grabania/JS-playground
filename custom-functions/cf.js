9e76c97e46e1c0ae2f5302626c0c6d4b3e716474
grabania <grabania.work@gmail.com>
2020-07-18 23:31:59 +0100

    Add variables to the function

--- custom-functions/cf.js
+++ custom-functions/cf.js
@@ -1,11 +1,16 @@
 //Function Definition
+let bill = 100;
+const taxRate = 1.13;
+
 function calculateBill() {
 	//this is the function body(or block)
 	console.log('Running Calculate Bill!!');
-	const total = 100 * 1.13;
+	const total = bill * 1 + taxRate;
 	return total;
 }
 
 //Function Call or Function Run
 const myTotal = calculateBill();
-console.log(`Your Total is ${myTotal}`);
+bill = 200;
+const myTotal2 = calculateBill();
+console.log(myTotal, myTotal2);
