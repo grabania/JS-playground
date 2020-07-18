57430e872bb473c185b9e23ab571e539a64cf7c7
grabania <grabania.work@gmail.com>
2020-07-18 23:37:44 +0100

    Add parameters and arguments

--- custom-functions/cf.js
+++ custom-functions/cf.js
@@ -1,16 +1,12 @@
 //Function Definition
-let bill = 100;
-const taxRate = 1.13;
-
-function calculateBill() {
+function calculateBill(billAmount, taxRate) {
 	//this is the function body(or block)
 	console.log('Running Calculate Bill!!');
-	const total = bill * 1 + taxRate;
+	const total = billAmount * 1 + taxRate;
 	return total;
 }
 
 //Function Call or Function Run
-const myTotal = calculateBill();
-bill = 200;
-const myTotal2 = calculateBill();
+const myTotal = calculateBill(100, 0.13);
+const myTotal2 = calculateBill(200, 0.13);
 console.log(myTotal, myTotal2);
