b7ad8de2545742095c560277890a53e9c88a9e42
grabania <grabania.work@gmail.com>
2020-07-20 23:51:40 +0100

    Add anon function and function expression

---
+++ custom-functions/ways-to-make-a-function.js
@@ -0,0 +1,13 @@
+// function doctorize(firstName) {
+// 	return `Dr. ${firstName}`;
+// }
+
+//Anon Function
+// function (firstName) {
+//     return `Dr. ${firstName}`;
+// }
+
+//Function Expression
+const doctorize = function(firstName) {
+	return `Dr. ${firstName}`;
+};
