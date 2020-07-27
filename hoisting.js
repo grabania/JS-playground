f7b6332821ea51f8614e756e65eef6d2e72c3a7c
grabania <grabania.work@gmail.com>
2020-07-27 23:54:27 +0100

    Add sayHi and add functions

---
+++ hoisting.js
@@ -0,0 +1,12 @@
+/* What does this file do? */
+sayHi();
+
+/* How does this file do it? */
+function sayHi() {
+	console.log('hey!');
+	console.log(add(10, 2));
+}
+
+function add(a, b) {
+	return a + b;
+}
