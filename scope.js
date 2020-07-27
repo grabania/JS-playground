b40d8737c4565c4b3d6dc090609acc3e399dad95
grabania <grabania.work@gmail.com>
2020-07-27 23:38:11 +0100

    Add logDog and go functions

--- scope.js
+++ scope.js
@@ -9,17 +9,31 @@
 // }
 
 // go();
-let cool;
 
-function isCool(name) {
-	let cool;
-	if (name === 'me') {
-		cool = true;
-	}
-	console.log(cool);
-	return cool;
+// let cool;
+
+// function isCool(name) {
+// 	let cool;
+// 	if (name === 'me') {
+// 		cool = true;
+// 	}
+// 	console.log(cool);
+// 	return cool;
+// }
+
+// for (let i = 0; i < 10; i++) {
+// 	console.log(i);
+// }
+
+const dog = 'snickers';
+
+function logDog() {
+	console.log(dog);
 }
 
-for (let i = 0; i < 10; i++) {
-	console.log(i);
+function go() {
+	const dog = 'sunny';
+	logDog();
 }
+
+go();
