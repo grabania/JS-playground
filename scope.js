fe958ccd07c96445ad732082961dac05b2eddf1a
grabania <grabania.work@gmail.com>
2020-07-27 23:32:42 +0100

    Add function isCool

--- scope.js
+++ scope.js
@@ -1,13 +1,21 @@
-const age = 100;
+// const age = 100;
 
-function go() {
-	const hair = 'blonde';
-	myAge = 200;
-	console.log(hair);
-	console.log(age);
-	console.log(myAge);
+// function go() {
+// 	const hair = 'blonde';
+// 	myAge = 200;
+// 	console.log(hair);
+// 	console.log(age);
+// 	console.log(myAge);
+// }
+
+// go();
+let cool;
+
+function isCool(name) {
+	let cool;
+	if (name === 'me') {
+		cool = true;
+	}
+	console.log(cool);
+	return cool;
 }
-
-go();
-
-console.log(age);
